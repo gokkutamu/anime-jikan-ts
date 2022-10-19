94c405e354cb5aae6175d46295c58a54de1319a9
Tran Ngoc Tam <tran.ngoc.tam@uruca.jp>
2022-10-19 16:27:00 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { Provider } from "react-redux";
+
+// const queryClient = new QueryClient();
+
+ReactDOM.render(
+    <BrowserRouter>
+        
+    </BrowserRouter>,
+    document.getElementById('root')
+);
